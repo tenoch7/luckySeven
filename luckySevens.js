2345b5745fd65f7db69d7a3e97fbd8d34cf5437c
Juan <juanmoralestowns@gmail.com>
2018-04-02 15:08:05 -0500

    Refactor function Play

--- luckySevens.js
+++ luckySevens.js
@@ -1,21 +1,6 @@
-
-function rollDice() {
-    return Math.ceil(Math.random() * (1 + 6 - 1));
-}
-
-function validateItems() {
-    var bet = document.forms["betting"]["startingBet"].value;
-    if (bet == "" || bet == 0 || bet < 0 || isNaN(bet)){
-        alert("Starting Bet can't be less or equal to 0.");
-        document.forms["betting"]["startingBet"].parentElement.className = "form-group has-error";
-        document.forms["betting"]["startingBet"].focus();
-        return false;
-    }
-}
-
 function play() {
     validateItems();
-        
+    
     var bet = document.forms["betting"]["startingBet"].value;
     var startingBet = bet;
     var highestAmount = bet;
@@ -36,16 +21,32 @@ function play() {
             bet--;
         }
     }
+    displayResultTable(startingBet, totalRolls, highestAmount, highestAmountRoll);
+    return false;
+    
+} 
+
+
+function validateItems() {
+    var bet = document.forms["betting"]["startingBet"].value;
+    if (bet == "" || bet == 0 || bet < 0 || isNaN(bet)){
+        alert("Starting Bet can't be less or equal to 0.");
+        document.forms["betting"]["startingBet"].parentElement.className = "form-group has-error";
+        document.forms["betting"]["startingBet"].focus();
+        return false;
+    }
+}
+
+function rollDice() {
+    return Math.ceil(Math.random() * (1 + 6 - 1));
+}
+
+
+function displayResultTable(startingBet, totalRolls, highestAmount, highestAmountRoll){
     document.getElementById("resultTable").style.display = "block";
     document.getElementById("resultHeading").innerText = "Results";
     document.getElementById("resultStartingBet").innerText = "Starting Bet " + startingBet;
     document.getElementById("resultRollsBeforeBroke").innerText = "Total Rolls Before Broke " + totalRolls;
     document.getElementById("resultHighestWon").innerText = "Highest Amount Won " + highestAmount;
-    document.getElementById("resultRollsHighestAmount").innerText = "Roll Count at Highest Amount Won " +highestAmountRoll ;
-    
-    return false;
-
-} 
-
-
-
+    document.getElementById("resultRollsHighestAmount").innerText = "Roll Count at Highest Amount Won " + highestAmountRoll;
+}
